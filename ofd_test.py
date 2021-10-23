4c93d2e00cc71b3232fe4ac65951db72954acf87
Eric Qian <lavave@gmail.com>
2021-10-23 22:25:11 +0200

    ofd_test: print out PNGs' paths; merge PNGs into a PDF in the end

--- ofd_test.py
+++ ofd_test.py
@@ -1,11 +1,18 @@
 from core.document import OFDFile
 import os
+import img2pdf
 
 folder = "ofds"
 for path in os.listdir(folder):
     if not path.endswith(".ofd"):
         continue
-    print("read file", path)
     file_path = os.path.join(folder, path)
+    print(f"> Reading from OFD: {file_path}")
     doc = OFDFile(file_path)
-    doc.draw_document(destination=folder)
+    png_paths = [p.as_posix() for p in doc.draw_document(destination=folder)]
+    print(f"> Converted PNG(s):")
+    print("\n".join(png_paths))
+    pdf_path = os.path.join(folder, path.replace(".ofd", ".pdf"))
+    print(f"> Writing to PDF: {pdf_path}")
+    with open(pdf_path, 'wb') as pdf:
+        pdf.write(img2pdf.convert(png_paths))
